89360babd08d467061925d2bccb01d26833044ae
vidit kushwaha <viditkushwaha530@gmail.com>
2024-02-06 21:43:43 +0530

    added pwa feature

--- next.config.js
+++ next.config.js
@@ -1,25 +1,33 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})
+
 const nextConfig = {
-    experimental: {
-        esmExternals: "loose", 
-        serverComponentsExternalPackages: ["mongoose"] 
+  experimental: {
+    esmExternals: 'loose',
+    serverComponentsExternalPackages: ['mongoose'],
+  },
+  webpack: (config) => {
+    config.experiments = {
+      layers: true,
+      topLevelAwait: true,
+    }
+    return config
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        pathname: '**',
+        hostname: '**',
       },
-      webpack: (config) => {
-        config.experiments = {
-            layers: true,
-          topLevelAwait: true
-        };
-        return config;
-      },
-      images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            pathname: '**',
-            hostname: '**'
-          },
-        ],
-      },
+    ],
+  },
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
